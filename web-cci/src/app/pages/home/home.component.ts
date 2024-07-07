512c14a85cb56e01627f8d221731c21fe5a7ec25
Guigna-hub <99ignaciogutierrez@gmail.com>
2024-07-07 20:42:41 -0300

    Update home page layout and styles

--- web-cci/src/app/pages/home/home.component.ts
+++ web-cci/src/app/pages/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('containerOuter02') containerOuterRef!: ElementRef;
+  @ViewChild('textContainer02') textContainerRef!: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    this.adjustContainerHeight();
+  }
+
+  adjustContainerHeight(): void {
+    const textContainerHeight = this.textContainerRef.nativeElement.offsetHeight;
+    this.containerOuterRef.nativeElement.style.minHeight = `${textContainerHeight}px`;
+  }
 }
